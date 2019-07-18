7feb84df543e0cd747edcecdcd3d77ae350b1ead
newtypeV2 <gianlibunao@gmail.com>
2019-07-18 14:22:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
+
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={()=>{console.log("BORED")}} />
   </div>,
   document.getElementById('root')
 );
+
